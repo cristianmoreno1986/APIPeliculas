c3bdd9e7b547eda89f7a4040d74c3900875485d0
Cristian Moreno C <cristian.moreno1986@outlook.com>
2023-07-27 16:47:05 -0500

    Inicializacion de RESTAPI Render2WEB

---
+++ ApiPeliculas/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiPeliculas.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPeliculas.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+        }
+
+        //Agregar los modelos aquí
+        public DbSet<Categoria> Categoria { get; set; }
+    }
+}
